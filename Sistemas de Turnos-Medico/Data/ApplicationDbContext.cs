3ee9ad39091c4ae80e14ee99d043d1e9e00a2f6a
Omar Moyano <omarmoyano7w@gmail.com>
2023-10-28 11:18:34 -0300

    Add project files.

---
+++ Sistemas de Turnos-Medico/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sistemas_de_Turnos_Medico.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

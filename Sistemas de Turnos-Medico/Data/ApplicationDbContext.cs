c3975c243fdf63f931485af4033ae0c6df333e0c
Omar Moyano <omarmoyano7w@gmail.com>
2023-10-29 18:42:40 -0300

    Update ApplicationDbContext.cs

--- Sistemas de Turnos-Medico/Data/ApplicationDbContext.cs
+++ Sistemas de Turnos-Medico/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Sistemas_de_Turnos_Medico.Models;
 
 namespace Sistemas_de_Turnos_Medico.Data
 {
@@ -9,5 +10,9 @@ namespace Sistemas_de_Turnos_Medico.Data
             : base(options)
         {
         }
+
+        public DbSet<Doctor> Doctores { get; set; }
+        public DbSet<Turno> Turnos { get; set; }
+        public DbSet<Especializacion> Especializaciones { get; set; }
     }
 }

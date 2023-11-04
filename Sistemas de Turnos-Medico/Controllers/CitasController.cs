522c29062a03d3597df7d5aa2075bde12630dc8f
Omar Moyano <omarmoyano7w@gmail.com>
2023-11-04 13:56:04 -0300

    add: crud EstadoCitas y Citas

---
+++ Sistemas de Turnos-Medico/Controllers/CitasController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sistemas_de_Turnos_Medico.Data;
+using Sistemas_de_Turnos_Medico.Models;
+
+namespace Sistemas_de_Turnos_Medico.Controllers
+{
+    public class CitasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CitasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Citas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Citas.Include(c => c.Doctor).Include(c => c.Estado).Include(c => c.Paciente);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Citas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .Include(c => c.Doctor)
+                .Include(c => c.Estado)
+                .Include(c => c.Paciente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // GET: Citas/Create
+        public IActionResult Create()
+        {
+            ViewData["DoctorId"] = new SelectList(_context.Doctores, "Id", "Apellido");
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Descripcion");
+            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Apellido");
+            return View();
+        }
+
+        // POST: Citas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Fecha,Hora,EstadoId,DoctorId,PacienteId")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cita);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DoctorId"] = new SelectList(_context.Doctores, "Id", "Apellido", cita.DoctorId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Descripcion", cita.EstadoId);
+            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Apellido", cita.PacienteId);
+            return View(cita);
+        }
+
+        // GET: Citas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            ViewData["DoctorId"] = new SelectList(_context.Doctores, "Id", "Apellido", cita.DoctorId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Descripcion", cita.EstadoId);
+            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Apellido", cita.PacienteId);
+            return View(cita);
+        }
+
+        // POST: Citas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Fecha,Hora,EstadoId,DoctorId,PacienteId")] Cita cita)
+        {
+            if (id != cita.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cita);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitaExists(cita.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DoctorId"] = new SelectList(_context.Doctores, "Id", "Apellido", cita.DoctorId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Descripcion", cita.EstadoId);
+            ViewData["PacienteId"] = new SelectList(_context.Pacientes, "Id", "Apellido", cita.PacienteId);
+            return View(cita);
+        }
+
+        // GET: Citas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .Include(c => c.Doctor)
+                .Include(c => c.Estado)
+                .Include(c => c.Paciente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // POST: Citas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Citas == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Citas'  is null.");
+            }
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita != null)
+            {
+                _context.Citas.Remove(cita);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitaExists(int id)
+        {
+          return (_context.Citas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Sistemas de Turnos-Medico/Controllers/EstadoCitasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sistemas_de_Turnos_Medico.Data;
+using Sistemas_de_Turnos_Medico.Models;
+
+namespace Sistemas_de_Turnos_Medico.Controllers
+{
+    public class EstadoCitasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EstadoCitasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EstadoCitas
+        public async Task<IActionResult> Index()
+        {
+              return _context.Estados != null ? 
+                          View(await _context.Estados.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Estados'  is null.");
+        }
+
+        // GET: EstadoCitas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Estados == null)
+            {
+                return NotFound();
+            }
+
+            var estadoCita = await _context.Estados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (estadoCita == null)
+            {
+                return NotFound();
+            }
+
+            return View(estadoCita);
+        }
+
+        // GET: EstadoCitas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EstadoCitas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descripcion")] EstadoCita estadoCita)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(estadoCita);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(estadoCita);
+        }
+
+        // GET: EstadoCitas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Estados == null)
+            {
+                return NotFound();
+            }
+
+            var estadoCita = await _context.Estados.FindAsync(id);
+            if (estadoCita == null)
+            {
+                return NotFound();
+            }
+            return View(estadoCita);
+        }
+
+        // POST: EstadoCitas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descripcion")] EstadoCita estadoCita)
+        {
+            if (id != estadoCita.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(estadoCita);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EstadoCitaExists(estadoCita.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(estadoCita);
+        }
+
+        // GET: EstadoCitas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Estados == null)
+            {
+                return NotFound();
+            }
+
+            var estadoCita = await _context.Estados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (estadoCita == null)
+            {
+                return NotFound();
+            }
+
+            return View(estadoCita);
+        }
+
+        // POST: EstadoCitas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Estados == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Estados'  is null.");
+            }
+            var estadoCita = await _context.Estados.FindAsync(id);
+            if (estadoCita != null)
+            {
+                _context.Estados.Remove(estadoCita);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EstadoCitaExists(int id)
+        {
+          return (_context.Estados?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

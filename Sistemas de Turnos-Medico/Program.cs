2a1e1ee8a4fc2db8475ecc337cfeb4e23558ad34
Omar Moyano <omarmoyano7w@gmail.com>
2023-11-14 16:23:04 -0300

    Configuration to deploy

--- Sistemas de Turnos-Medico/Program.cs
+++ Sistemas de Turnos-Medico/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseMigrationsEndPoint();
 }
